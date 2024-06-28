70f106589cc7c2773f1e4596310fe9b700d23ca9
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-28 11:02:47 -0300

    Conversões e outros tipos

---
+++ SintaxeVariaveisFluxo/src/testaVariaveis/TestaConversao.java
@@ -0,0 +1,23 @@
+package testaVariaveis;
+
+public class TestaConversao {
+	
+	public static void main(String[] args) {
+		
+		float pontoFlutuante = (float) 3.14;
+		float pontoFlutuante2 = 3.14f;
+		
+		double salario = 1270.50;
+		int valor = (int) salario; // O casting faz a conversão quando ela não é possível de forma automática.
+		System.out.println(valor);	
+		
+		long numeroGrande = 6454646467l;
+		short valorPequeno = 5555;
+		byte b = 127;
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		System.out.println(total);
+	}
+}
